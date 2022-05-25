f5c78744645ce10f4f14802336b265c8609c45b8
kinau <github@mrkinaujr.xyz>
2022-05-25 14:14:35 +0200

    added navbar

---
+++ Frontend/src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Menubar :model="routes">
+      <template #start>
+        <img alt="logo" src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" height="40" class="mr-2">
+      </template>
+
+    </Menubar>
+  </div>
+</template>
+
+<script>
+import Menubar from 'primevue/menubar';
+import router from "@/router";
+
+export default {
+  name: "Navbar",
+  data: function () {
+    return {
+
+    }
+  },
+  computed: {
+    routes() {
+      var items = [];
+      router.routes.forEach((route)=>{
+        items.push({
+          label: route.name,
+          to: route.path
+        })
+      })
+      return items;
+    }
+  },
+  components: {
+    Menubar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Menubar from "primevue/menubar";
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router.router).use(PrimeVue).component("Menubar",Menubar).mount('#app')
